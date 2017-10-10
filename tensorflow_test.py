0696c1847365074015b52685eb462b64b507d522
raincrash <sricharanized@gmail.com>
2017-10-10 15:36:20 +0530

    Adding the temporary dump

--- tensorflow_test.py
+++ tensorflow_test.py
@@ -5,7 +5,7 @@ import numpy as np
 x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data * 0.1 + 0.3
 
-
+# Weight and bias
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.zeros([1]))
 
@@ -13,6 +13,8 @@ y = W * x_data + b
 
 loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
+
+
 train = optimizer.minimize(loss)
 
 init = tf.global_variables_initializer()
